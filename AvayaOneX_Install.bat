6f5b8cbde60559fc35fa13bd2f24f6520c68ee1c
Pinnaclenetworking <61787099+Pinnaclenetworking@users.noreply.github.com>
2020-07-01 08:02:50 -0400

    Update AvayaOneX_Install.bat

--- AvayaOneX_Install.bat
+++ AvayaOneX_Install.bat
@@ -6,10 +6,10 @@ color 0A
 @echo.
 @echo User ID: %2
 
-xcopy /s /i \\wseng\updates\Local\AVAYAONEX_Install \\%1\C$\zco\AVAYAONEX_Install
+xcopy /s /i \\<server name>\<folder>\Local\AVAYAONEX_Install \\%1\C$\zco\AVAYAONEX_Install
 
 Start "AVAYAONEX 2-5-10 Install on %1 for %2" "C:\Program Files (x86)\rts\plugins\CMDPrompt\psexec.exe" \\%1 "cmd.exe" "/k powershell c:\ZCO\AVAYAONEX_Install\avayaonex_2-5-10_install.ps1"
 
-RD \\%1\C$\zco\AVAYAONEX_Install /s /q
+RD \\%1\C$\<copyto location>\AVAYAONEX_Install /s /q
 
 
